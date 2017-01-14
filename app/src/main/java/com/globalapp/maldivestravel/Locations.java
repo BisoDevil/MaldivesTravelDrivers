f8f85d2fd23d97738966e956071e326d7837ce7c
BisoDevil <bisodevil@gmail.com>
2017-01-14 20:40:01 +0200

    initialize

--- app/src/main/java/com/globalapp/maldivestravel/Locations.java
+++ app/src/main/java/com/globalapp/maldivestravel/Locations.java
@@ -145,12 +145,7 @@ public class Locations extends Service {
                         != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getApplicationContext(),
                         android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                     // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
+
                     return;
                 }
                 GPS = Locationmanager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
